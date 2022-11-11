7b38b6d191f807159e2c87623aa3fce66ca9fed6
Andrea Lam <70552370+andreadelou@users.noreply.github.com>
2022-11-10 19:33:35 -0600

    Update mv.py.py

--- mv.py.py
+++ mv.py.py
@@ -1,8 +1,10 @@
+ 
 import numpy
 import random
 import pygame
 from OpenGL.GL import *
 from OpenGL.GL.shaders import *
+import glm
 
 pygame.init()
 
@@ -10,15 +12,23 @@ screen = pygame.display.set_mode(
     (1600, 1200),
     pygame.OPENGL | pygame.DOUBLEBUF
 )
+# dT = pygame.time.Clock()
+
 
 
 vertex_shader = """
 #version 460
 layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 vertexColor;
+
+out vec3 ourColor;
+
 
 void main()
 {
-    gl_Position = vec4(position.x, position.y, position.z, 1.0f);
+    gl_Position = vec4(position, 1.0f);
+    ourColor = vertexColor;
+
 }
 """
 
@@ -27,9 +37,15 @@ fragment_shader = """
 
 layout (location = 0) out vec4 fragColor;
 
+uniform vec3 color;
+
+
+in vec3 ourColor;
+
 void main()
 {
-    fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    // fragColor = vec4(ourColor, 1.0f);
+    fragColor = vec4(color, 1.0f);
 }
 """
 compiled_vertex_shader = compileShader(vertex_shader, GL_VERTEX_SHADER)
@@ -38,13 +54,15 @@ shader = compileProgram(
     compiled_vertex_shader,
     compiled_fragment_shader
 )
+
 glUseProgram(shader)
 
 
+
 vertex_data = numpy.array([
-    -0.5, -0.5, 0.0,
-     0.5, -0.5, 0.0,
-     0.0,  0.5, 0.0,
+    -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
+     0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+     0.0,  0.5, 0.0, 0.0, 0.0, 1.0
 ], dtype=numpy.float32)
 
 vertex_buffer_object = glGenBuffers(1)
@@ -63,10 +81,21 @@ glVertexAttribPointer(
     3,
     GL_FLOAT,
     GL_FALSE,
-    3 * 4,
+    6 * 4,
     ctypes.c_void_p(0)
 )
 glEnableVertexAttribArray(0)
+
+
+glVertexAttribPointer(
+    1,
+    3,
+    GL_FLOAT,
+    GL_FALSE,
+    6 * 4,
+    ctypes.c_void_p(3 * 4)
+)
+glEnableVertexAttribArray(1)
 
 
 
@@ -78,6 +107,21 @@ glClearColor(0.5, 1.0, 0.5, 1.0)
 
 while running:
     glClear(GL_COLOR_BUFFER_BIT)
+
+    color1 = random.random()
+    color2 = random.random()
+    color3 = random.random()
+
+    color = glm.vec3(color1, color2, color3)
+
+    glUniform3fv(
+        glGetUniformLocation(shader,'color'),
+        1,
+        glm.value_ptr(color)
+    )
+
+    pygame.time.wait(100)
+
 
     glDrawArrays(GL_TRIANGLES, 0, 3)
 
